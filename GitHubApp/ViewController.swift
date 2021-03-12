1fcc709a82d69ffd48a07dc756c6af71e4bc2f24
Marko Zivko <marko.zivko@sap.com>
2021-03-12 19:04:22 +0100

    Create window scene used for our app window and set some background color to the view

--- GitHubApp/ViewController.swift
+++ GitHubApp/ViewController.swift
@@ -12,9 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("")
+        self.view.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
     }
-
-
 }
 
