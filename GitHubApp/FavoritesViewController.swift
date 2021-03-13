2bde38654c4e1cf4f561dc33710c7bb7dd2ac70d
Marko Zivko <marko.zivko@sap.com>
2021-03-13 22:24:27 +0100

    Create tabBar and Navigation controllers for the app, and customize them

---
+++ GitHubApp/FavoritesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesViewController.swift
+//  GitHubApp
+//
+//  Created by Zivko, Marko on 13/03/2021.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemGreen
+    }
+}
---
+++ GitHubApp/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GitHubApp
+//
+//  Created by Zivko, Marko on 13/03/2021.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBlue
+
+    }
+}
