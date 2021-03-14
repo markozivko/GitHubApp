7f2d6bad8bc9cb6a67b831d96e48f7776d5530ad
Marko Zivko <marko.zivko@sap.com>
2021-03-14 20:47:31 +0100

    Configure constraints and add button, imageLogo and text field to the SearchVC

--- GitHubApp/SearchViewController.swift
+++ GitHubApp/SearchViewController.swift
@@ -9,10 +9,60 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .systemBlue
+        self.view.backgroundColor = .systemBackground
+        self.configureLogoImageView()
+        self.configureTextField()
+        self.configureCallToActionButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView() {
+        self.view.addSubview(self.logoImageView)
 
+        self.logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            self.logoImageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            self.logoImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            self.logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextField() {
+        self.view.addSubview(self.usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            self.usernameTextField.topAnchor.constraint(equalTo: self.logoImageView.bottomAnchor, constant: 48),
+            self.usernameTextField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            self.usernameTextField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50),
+            self.usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton() {
+        
+        self.view.addSubview(self.callToActionButton)
+        
+        NSLayoutConstraint.activate([
+            self.callToActionButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            self.callToActionButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            self.callToActionButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50),
+            self.callToActionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
     }
 }
