37e9b077b0fc2394bb2f88cc968ab5910875412d
Marko Zivko <marko.zivko@sap.com>
2021-03-18 09:59:26 +0100

    Create network manager and decode json from the response from server

--- GitHubApp/Controllers/FollowersListViewController.swift
+++ GitHubApp/Controllers/FollowersListViewController.swift
@@ -16,6 +16,16 @@ class FollowersListViewController: UIViewController {
 
         self.view.backgroundColor = .systemBackground
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("Number of followers: \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
---
+++ GitHubApp/NetworkManager/NetworkManager.swift
@@ -0,0 +1,56 @@
+//
+//  NetworkManager.swift
+//  GitHubApp
+//
+//  Created by Zivko, Marko on 18/03/2021.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    let baseURL = "https://api.github.com/users/"
+    
+    private init() {
+        
+    }
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping([Follower]?, String?) -> Void)  {
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(nil, "This username created an invalid request, please try again.")
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(nil, "Unable to complete your request. Please check your internet connection.")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(nil, "Invalid response from the server, please try again")
+                return
+            }
+            
+            guard let data = data else {
+                completed(nil, "The data received from the server is invalid, try again.")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(followers, nil)
+            } catch {
+                completed(nil, "The data received from the server is invalid, try again.")
+            }
+        }
+        
+        task.resume()
+    }
+    
+}
