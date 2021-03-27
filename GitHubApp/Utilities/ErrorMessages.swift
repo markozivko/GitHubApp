72c3413ab965a1e6edbd264cfbd9eb369ebbcecd
Marko Zivko <marko.zivko@sap.com>
2021-03-27 13:12:27 +0100

    Refactor error messaging and create Enum for all messages to stay in one place

---
+++ GitHubApp/Utilities/ErrorMessages.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorMessages.swift
+//  GitHubApp
+//
+//  Created by Zivko, Marko on 27/03/2021.
+//
+
+import Foundation
+
+enum ErrorMessages: String {
+    
+    case invalidUsername = "This username created an invalid request, please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server, please try again"
+    case invalidData = "The data received from the server is invalid, try again."
+    
+}
