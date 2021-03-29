ee1ac0c57caf9804b48010203fad90f96d941d43
Marko Zivko <marko.zivko@sap.com>
2021-03-29 14:05:44 +0200

    Create and call network call for getting single user data

--- GitHubApp/Controllers/UserInfoViewController.swift
+++ GitHubApp/Controllers/UserInfoViewController.swift
@@ -21,6 +21,17 @@ class UserInfoViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = doneButton
         
         self.title = self.username
+        
+        guard let username = self.username else { return }
+        NetworkManager.shared.getUserInfo(for: username, completed: { [weak self]result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        })
     }
     
     @objc func donePressed() {
