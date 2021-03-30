dcc57e9b47b3e2e7b5a5ab47f9d7d832ad4ae8ec
Marko Zivko <marko.zivko@sap.com>
2021-03-30 11:27:19 +0200

    Add itemInfoViews below the header view and refactor code

--- GitHubApp/Controllers/UserInfoViewController.swift
+++ GitHubApp/Controllers/UserInfoViewController.swift
@@ -10,19 +10,21 @@ import UIKit
 class UserInfoViewController: UIViewController {
 
     let headerView = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+    
     var username: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = .systemBackground
+        
+        self.configureViewController()
         self.configure()
-        
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(donePressed))
-        self.navigationItem.rightBarButtonItem = doneButton
-        
-        self.title = self.username
-        
+        self.getUserDataInfo()
+
+    }
+    
+    func getUserDataInfo() {
         guard let username = self.username else { return }
         NetworkManager.shared.getUserInfo(for: username, completed: { [weak self]result in
             guard let self = self else { return }
@@ -36,19 +38,47 @@ class UserInfoViewController: UIViewController {
                 self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         })
+
+    }
+    
+    func configureViewController() {
+        self.view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(donePressed))
+        self.navigationItem.rightBarButtonItem = doneButton
+        self.title = self.username
     }
     
     func configure() {
         self.view.addSubview(self.headerView)
+        self.view.addSubview(self.itemViewOne)
+        self.view.addSubview(self.itemViewTwo)
+        
+        let padding: CGFloat = 20
         
         self.headerView.layer.cornerRadius = 10
         self.headerView.translatesAutoresizingMaskIntoConstraints = false
+        self.itemViewOne.translatesAutoresizingMaskIntoConstraints = false
+        self.itemViewTwo.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.itemViewOne.backgroundColor = .systemOrange
+        self.itemViewTwo.backgroundColor = .systemBlue
         
         NSLayoutConstraint.activate([
             self.headerView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
             self.headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
             self.headerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            self.headerView.heightAnchor.constraint(equalToConstant: 180)
+            self.headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            self.itemViewOne.topAnchor.constraint(equalTo: self.headerView.bottomAnchor, constant: padding),
+            self.itemViewOne.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: padding),
+            self.itemViewOne.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -padding),
+            self.itemViewOne.heightAnchor.constraint(equalToConstant: 140),
+            
+            self.itemViewTwo.topAnchor.constraint(equalTo: self.itemViewOne.bottomAnchor, constant: padding),
+            self.itemViewTwo.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: padding),
+            self.itemViewTwo.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -padding),
+            self.itemViewTwo.heightAnchor.constraint(equalToConstant: 140),
+            
         ])
     }
     
